676a04e239d1d74fd099bcfcf0e8345cc3d9dcdd
Nicole Graiff <nicole.graiff@gmail.com>
2020-09-30 00:21:27 -0400

    still trying....

--- src/App.js
+++ src/App.js
@@ -14,12 +14,8 @@ function App() {
         <Navbar />
         {/*<Wrapper>*/}
           <Switch>
-            <Route exact path={"/nicole-graiff-portfolio/"}>
-              <Home />
-            </Route>
-            <Route exact path={"/nicole-graiff-portfolio/portfolio"}>
-             <Portfolio />
-            </Route>
+            <Route exact path="/nicole-graiff-portfolio" component={Home} />
+            <Route exact path="/nicole-graiff-portfolio/portfolio" component={Portfolio} />
             {/* <Route exact path="/nicole-graiff-portfolio/contact" component={Contact} /> */}
           {/*</Wrapper>*/}
           </Switch>
